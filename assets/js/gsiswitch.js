6d2893abb3cb35265e273b73e2c4dde02cc67d0a
Thorsten Zoerner <me@thorsten-zoerner.com>
2024-04-30 17:57:27 +0200

    auto

--- assets/js/gsiswitch.js
+++ assets/js/gsiswitch.js
@@ -102,7 +102,7 @@ $(document).ready(function() {
                 $('#allSwitches').html(table);
                 $('#trackerNav').append('<li class="nav-item text-center"><a class="nav-link" href="#gsiSwitch"><svg style="color: #147a50;" class="bi bi-toggles" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16"><path d="M4.5 9a3.5 3.5 0 1 0 0 7h7a3.5 3.5 0 1 0 0-7zm7 6a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5m-7-14a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5m2.45 0A3.49 3.49 0 0 1 8 3.5 3.49 3.49 0 0 1 6.95 6h4.55a2.5 2.5 0 0 0 0-5zM4.5 0h7a3.5 3.5 0 1 1 0 7h-7a3.5 3.5 0 1 1 0-7"></path></svg><br/>Schalter</a></li>');
             } else {
-                $('#allSwitches').html("Auf den Balken einer Uhrzeit des GrünstromIndex klicken, um einen Schalter zu erstellen.");
+                $('#allSwitches').html("Auf den Balken einer Uhrzeit des GrünstromIndex eines Ortes klicken, um einen Schalter zu erstellen.");
             }
           $('#switchExist').on('change',function() {
                 $('#switchName').val($('#switchExist').val()); 
