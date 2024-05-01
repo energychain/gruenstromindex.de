5ed1b45c9fb3a16ade272ffba856e1d5e9d43cff
Thorsten Zoerner <me@thorsten-zoerner.com>
2024-05-01 02:48:48 +0200

    auto

--- assets/js/gsitracker.js
+++ assets/js/gsitracker.js
@@ -56,6 +56,13 @@ const handleReadingButtonEvents = function() {
           $('#qrcode').html('');
           $('#presentJWTContent').val(data2.jwt);
           $('#presentJSON').val(JSON.stringify(data2.json, undefined, 4));
+          
+         let text ="<blockquote>Für die Inhaber der Kennung <abbr class='text-primary' title='"+data2.json.holder+"'>"+data2.json.holder.substring(0,6)+"...</abbr> wird ";
+         text += "ein Stromverbrauch unter der Kennung <abbr class='text-primary' title='"+data2.json.sub+"'>"+data2.json.sub.substring(0,6)+"...</abbr> von "+(data2.json.consumption/1000)+"kWh und eine Emission von "+(data2.json.emission/1000).toFixed(3).replace('.',',')+"kgCO<sub></sub> ";
+         text += "am "+new Date(data2.json.iat*1000).toLocaleString()+" durch den Inhaber der Kennung <abbr class='text-primary' title='"+data2.json.iss+"'>"+data2.json.iss.substring(0,6)+"...</abbr>  bestätigt ";
+         text += "für den Zeitraum von "+new Date(data2.json.nbf*1000).toLocaleString()+" bis "+new Date(data2.json.exp*1000).toLocaleString()+" mit der digitalen Signatur <abbr class='text-primary' title='"+data2.json.sig+"'>"+data2.json.sig.substring(0,6)+"...</abbr>  </blockquote."
+          text += "<br/><p class='text-muted'>"+""+"</p>";
+         $('#presentText').html(text);
           var qrcode = new QRCode(document.getElementById("qrcode"), {
             text: data2.jwt,
             width: 512,
