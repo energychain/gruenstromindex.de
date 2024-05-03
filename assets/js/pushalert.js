7c0bb395a042b48676a6ac14a7cc373da9f51e3e
Thorsten Zoerner <me@thorsten-zoerner.com>
2024-05-04 01:34:21 +0200

    auto

--- assets/js/pushalert.js
+++ assets/js/pushalert.js
@@ -1,9 +1,43 @@
 $(document).ready(function(){
+    function getUrlParameter(paramName) {
+        const urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get(paramName);
+    }
+
     const initOSID = function() {
         $('#oneSignalId').val(OneSignal.User.PushSubscription.id);
         if( $('#oneSignalId').val().length < 5) {
             setTimeout(initOSID, 1000);
         }
     }
+
+    $('#pushScheduler').on('submit', function(e) {
+        e.preventDefault();
+        const url = 'https://api.corrently.io/v2.0/scope2/pushScheduler';
+        
+        let startData = {
+          name:"Corrently GrünstromSchalter",
+          content:$('#sendMessage').val(),
+          after:new Date($('#sendAt').attr('data') * 1).toISOString().substring(0,16),
+          title:"Corrently GrünstromSchalter",
+          id:('#oneSignalId').val()
+        };
+        
+        fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(startData)
+        })
+        .then(response => response.json())
+        .then(data => {
+             location.href='./?#gsiTracker';
+        });
+
+    })
+    $('#sendMessage').val(getUrlParameter("switch")+" "+getUrlParameter("state"));
+    $('#sendAt').val(new Date(1 * getUrlParameter("time")).toLocaleString());
+    $('#sendAt').attr('data',getUrlParameter("time"));
     setTimeout(initOSID, 1000);
 });
