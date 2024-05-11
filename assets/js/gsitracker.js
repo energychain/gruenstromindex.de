e76569776d82cb043e5de425f388c98430b4456e
Thorsten Zoerner <me@thorsten-zoerner.com>
2024-05-11 13:54:41 +0200

    auto

--- assets/js/gsitracker.js
+++ assets/js/gsitracker.js
@@ -66,7 +66,7 @@ const trackerRowHTML = function (tracker, fromDelegation) {
 
   if ((tracker.did.aud == window.wallet.address) || (tracker.did.entity == window.wallet.address) || (tracker.ownerId == window.wallet.address)) {
 
-    html += '<td title="Eigentümer">';
+    html += '<td title="Eigentümer" class="text-nowrap">';
 
     html += '<svg class="bi bi-wallet" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">';
     html += '<path d="M0 3a2 2 0 0 1 2-2h13.5a.5.5 0 0 1 0 1H15v2a1 1 0 0 1 1 1v8.5a1.5 1.5 0 0 1-1.5 1.5h-12A2.5 2.5 0 0 1 0 12.5zm1 1.732V12.5A1.5 1.5 0 0 0 2.5 14h12a.5.5 0 0 0 .5-.5V5H2a1.99 1.99 0 0 1-1-.268M1 3a1 1 0 0 0 1 1h12V2H2a1 1 0 0 0-1 1"></path>';
