4d8f1ada64c38b72bbb7449a2180ba10964932e3
Thorsten Zoerner <me@thorsten-zoerner.com>
2024-05-13 18:50:52 +0200

    auto

--- assets/js/gsiwallet.js
+++ assets/js/gsiwallet.js
@@ -168,6 +168,28 @@ $(document).ready(function(){
             html5QrcodeScanner.render(onScanSuccess, onScanFailure);
         });
     }
+
+    $('#scanRecipient').on('click',function(e) {
+        $('#modalQR').modal('show');
+        
+        function onScanSuccess(decodedText, decodedResult) {
+          // handle the scanned code as you like, for example:  
+          $('#modalQR').modal('hide');
+          $('#recipient').val(decodedText);
+        }
+    
+        function onScanFailure(error) {
+          // handle scan failure, usually better to ignore and keep scanning.
+          // for example:
+          console.warn(`Code scan error = ${error}`);
+        }
+    
+        let html5QrcodeScanner = new Html5QrcodeScanner(
+          "reader",
+          { fps: 10, qrbox: { width: 250, height: 250 } },
+              /* verbose= */ false);
+        html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+    });
    
     $('#executeContract').on('submit',async function(e) {
         e.preventDefault();
